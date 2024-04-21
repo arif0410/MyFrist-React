949021765153b1c92fe21e75741daef1ea0d1719
arif0410 <arif.ibrahim0410@gmail.com>
2024-04-21 11:07:25 +0700

    simpan

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./MyStyle.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>
+  const [count, setCount] = useState[0];
+  useEffect(()=> {
+    
+  })
+  return (
+    <div className="flex bg-slate-300 items-center">
+      hajobel
+    </div>
+  )
 }
 
 export default App;
