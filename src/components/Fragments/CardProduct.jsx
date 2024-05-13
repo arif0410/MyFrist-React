29783f6838ce9a48e516701f1db3276ee492f81f
arif0410 <arif.ibrahim0410@gmail.com>
2024-05-13 09:02:28 +0700

    flight

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -35,7 +35,7 @@ const Footer = (props) =>{
     return(
         <div className="flex items-center justify-between px-5 pb-5 ">
             <span className="text-xl font-bold text-white">$ {" "} {price.toLocaleString('id-ID',{styles:'currency',currency:'USD'})}</span>
-            <Button className="w-full" onClick={() => hendleAddToCart(id)}>Beli</Button>
+            <Button className="w-full" onClick={() => {hendleAddToCart(id)}}>Beli</Button>
         </div>
     )
 }
